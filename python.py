f2a84025abe87c73f135b5d5ca10903f16be4337
Shailee29 <shaileekadia2912@gmail.com>
2023-06-27 12:10:40 +0530

    new branch first commit

--- python.py
+++ python.py
@@ -1,1 +1,12 @@
 print("helloooo")
+
+
+
+
+git branch new_branch
+git checkout new_branchname
+//add new file to folder
+git status
+git add.
+git commit -m "new branch first commit"
+git push -u origin new_branchname
